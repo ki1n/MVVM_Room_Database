89e2d7f53b85c73f29efbcd1d39ef9aa96953169
ki1n <turevno@gmail.com>
2020-10-28 12:32:40 +0600

    ListUsersAdapter.kt

--- app/src/main/java/com/example/nikolaiturev/mvvmroomdatabase/fragments/list/ListFragment.kt
+++ app/src/main/java/com/example/nikolaiturev/mvvmroomdatabase/fragments/list/ListFragment.kt
@@ -5,12 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.nikolaiturev.mvvmroomdatabase.R
+import com.example.nikolaiturev.mvvmroomdatabase.data.UserViewModel
 import com.example.nikolaiturev.mvvmroomdatabase.databinding.FragmentListBinding
 
 class ListFragment : Fragment() {
     private lateinit var binding: FragmentListBinding
+    private lateinit var mUserViewModel: UserViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,6 +24,19 @@ class ListFragment : Fragment() {
         // Inflate the layout for this fragment
         //  return  inflater.inflate(R.layout.fragment_list, container, false)
         binding = FragmentListBinding.inflate(layoutInflater)
+
+        // RecyclerView
+        val adapterUsers = ListUsersAdapter()
+        val recyclerView = binding.recyclerViewUsers
+        recyclerView.adapter = adapterUsers
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+
+        // UserViewModel
+        mUserViewModel = ViewModelProvider(this).get(UserViewModel::class.java)
+        // viewLifecycleOwner вместо this тк это жз цикл фрагмента
+        mUserViewModel.readAllData.observe(viewLifecycleOwner, Observer { users ->
+                adapterUsers.setData(users)
+        })
 
         binding.floatingActionButton.setOnClickListener {
             findNavController().navigate(R.id.action_listFragment_to_addFragment) // переход на другой фрагметн
---
+++ app/src/main/java/com/example/nikolaiturev/mvvmroomdatabase/fragments/list/ListUsersAdapter.kt
@@ -0,0 +1,48 @@
+package com.example.nikolaiturev.mvvmroomdatabase.fragments.list
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.nikolaiturev.mvvmroomdatabase.data.User
+import com.example.nikolaiturev.mvvmroomdatabase.databinding.CustomRowBinding
+
+class ListUsersAdapter : RecyclerView.Adapter<MyViewHolder>() {
+
+    private var userList = emptyList<User>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val binding =
+            CustomRowBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return MyViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val currentItem = userList[position]
+        holder.bind(currentItem)
+    }
+
+    override fun getItemCount(): Int {
+        return userList.size
+    }
+
+    fun setData(users: List<User>){
+        this.userList = users
+        notifyDataSetChanged()
+    }
+}
+
+class MyViewHolder(
+    private val binding: CustomRowBinding
+) : RecyclerView.ViewHolder(binding.root) {
+    fun bind(user: User) {
+        binding.apply {
+            idTxt.text = user.id.toString()
+            firstNameTxt.text = user.firstName
+            lastNameTxt.text = user.lastName
+            ageTxt.text = user.age.toString()
+        }
+    }
+}
+
+
+
